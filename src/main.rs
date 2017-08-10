9468cdd5ecded9ecd94689a9cc55d69865eb2b48
Skyler Hawthorne <skyler@opendns.com>
2017-08-10 16:19:23 -0700

    Provide options for encode set, lossy decoding
    * Adds a command line option for each encode set defined by the
      `percent_encoding` crate
    * Adds a flag for "strict" (non-lossy) decoding, and makes lossy the
      default encoding

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::io::BufRead;
 use std::iter;
 
 use clap::{Arg, App, ArgMatches};
-use percent_encoding::{utf8_percent_encode, percent_decode, PATH_SEGMENT_ENCODE_SET};
+use percent_encoding as pe;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -18,8 +18,30 @@ fn main() {
         .author("Skyler Hawthorne <skylerhawthorne@gmail.com>")
         .about("URL-encodes or -decodes the input")
         .arg(Arg::with_name("decode").short("d").long("decode").help(
-            "Decode the input, rather than encode",
+            "Decode the input, rather than encode.",
         ))
+        .arg(
+            Arg::with_name("strict-decode")
+                .short("s")
+                .long("strict-decode")
+                .help(
+                    "Decode the input non-lossily. If set, the program will fail if it \
+                     encounters a sequence that does not produce valid UTF-8.",
+                ),
+        )
+        .arg(
+            Arg::with_name("encode-set")
+                .short("e")
+                .long("encode-set")
+                .takes_value(true)
+                .possible_values(&["default", "path", "query", "simple", "userinfo"])
+                .default_value("default")
+                .help(
+                    "The encode set to use when encoding. See \
+                      https://docs.rs/percent-encoding/1.0.0/percent_encoding/index.html \
+                      for more details.",
+                ),
+        )
         .get_matches();
 
     if let Err(e) = run(&matches) {
@@ -34,19 +56,30 @@ fn run(arg_matches: &ArgMatches) -> Result<(), Box<Error + Send + Sync>> {
     let mut stdout_handle = stdout.lock();
     let mut stdin_handle = stdin.lock();
 
-    let decode_mode = arg_matches.is_present("decode");
+    let decode_mode = arg_matches.is_present("decode") || arg_matches.is_present("strict-decode");
+    let lossy = !arg_matches.is_present("strict-decode");
     let mut buf = String::new();
 
     while stdin_handle.read_line(&mut buf)? > 0 {
         // use an anonymous scope so `line`'s borrow gets dropped
         // before clearing the buffer
         {
-            let line = &buf[..buf.len() - 1];
+            let line = buf.trim_right();
 
             if decode_mode {
-                decode(line.as_bytes(), &mut stdout_handle)?;
+                decode(line.as_bytes(), &mut stdout_handle, lossy)?;
             } else {
-                encode(&line, &mut stdout_handle)?;
+                // Ugh, unfortunately, since EncodeSet : Cloned : Sized, it
+                // cannot be boxed, so it's impossible to choose our encode set
+                // only once.
+                match arg_matches.value_of("encode-set").unwrap() {
+                    "default" => encode(&line, pe::DEFAULT_ENCODE_SET, &mut stdout_handle)?,
+                    "path" => encode(&line, pe::PATH_SEGMENT_ENCODE_SET, &mut stdout_handle)?,
+                    "query" => encode(&line, pe::QUERY_ENCODE_SET, &mut stdout_handle)?,
+                    "simple" => encode(&line, pe::SIMPLE_ENCODE_SET, &mut stdout_handle)?,
+                    "userinfo" => encode(&line, pe::USERINFO_ENCODE_SET, &mut stdout_handle)?,
+                    _ => panic!("Unknown encode set"),
+                };
             }
         }
 
@@ -56,8 +89,19 @@ fn run(arg_matches: &ArgMatches) -> Result<(), Box<Error + Send + Sync>> {
     Ok(())
 }
 
-fn decode<W: io::Write>(line: &[u8], output: &mut W) -> Result<(), Box<Error + Send + Sync>> {
-    let decoded = percent_decode(line).decode_utf8()?;
+fn decode<W: io::Write>(
+    line: &[u8],
+    output: &mut W,
+    lossy: bool,
+) -> Result<(), Box<Error + Send + Sync>> {
+    let decoder = pe::percent_decode(line);
+
+    let decoded = if lossy {
+        decoder.decode_utf8_lossy()
+    } else {
+        decoder.decode_utf8()?
+    };
+
     let result = write_output(iter::once(decoded.borrow()), output);
 
     match result {
@@ -66,8 +110,12 @@ fn decode<W: io::Write>(line: &[u8], output: &mut W) -> Result<(), Box<Error + S
     }
 }
 
-fn encode<W: io::Write>(line: &str, output: &mut W) -> io::Result<()> {
-    let encoded = utf8_percent_encode(line, PATH_SEGMENT_ENCODE_SET);
+fn encode<W: io::Write, E: pe::EncodeSet>(
+    line: &str,
+    encode_set: E,
+    output: &mut W,
+) -> io::Result<()> {
+    let encoded = pe::utf8_percent_encode(line, encode_set);
     write_output(encoded, output)
 }
 
